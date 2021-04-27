a8ffbc5866a571413267f1913120281e2927197f
JSDale <57218102+JSDale@users.noreply.github.com>
2021-04-27 21:25:39 +0100

    button changes color in settings when selected

--- app/src/main/java/com/example/week9/network/jacobdale_com527_ae1/FragmentSettings.kt
+++ app/src/main/java/com/example/week9/network/jacobdale_com527_ae1/FragmentSettings.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 
 class FragmentSettings : Fragment()
@@ -23,8 +24,19 @@ class FragmentSettings : Fragment()
             val btnSaveLoadLocal = findViewById<Button>(R.id.buttondSaveAndLoadToFromLocalDb)
             val btnSaveLoadRemote = findViewById<Button>(R.id.buttondSaveAndLoadToFromRemoteDb)
 
-            btnSaveLoadLocal.setOnClickListener{Resources.saveToLocalDb = true}
-            btnSaveLoadRemote.setOnClickListener{Resources.saveToLocalDb = false}
+            btnSaveLoadLocal.setOnClickListener{Resources.saveToLocalDb = true
+                btnSaveLoadLocal.isClickable = false
+                btnSaveLoadRemote.isClickable=true
+                btnSaveLoadLocal.setBackgroundColor(ContextCompat.getColor(Resources.context,R.color.black))
+                btnSaveLoadRemote.setBackgroundColor(ContextCompat.getColor(Resources.context,R.color.purple_500))
+            }
+            btnSaveLoadRemote.setOnClickListener{
+                Resources.saveToLocalDb = false
+                btnSaveLoadLocal.isClickable = true
+                btnSaveLoadRemote.isClickable=false
+                btnSaveLoadLocal.setBackgroundColor(ContextCompat.getColor(Resources.context,R.color.purple_500))
+                btnSaveLoadRemote.setBackgroundColor(ContextCompat.getColor(Resources.context,R.color.black))
+            }
         }
     }
 }
