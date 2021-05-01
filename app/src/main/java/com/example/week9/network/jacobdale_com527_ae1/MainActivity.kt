6891bc1316375357bbfdff10ab70a41ab8eca17d
JSDale <57218102+JSDale@users.noreply.github.com>
2021-05-01 02:15:47 +0100

    added foundation for the new activity which will house 2 fragments for POI list and menu

--- app/src/main/java/com/example/week9/network/jacobdale_com527_ae1/MainActivity.kt
+++ app/src/main/java/com/example/week9/network/jacobdale_com527_ae1/MainActivity.kt
@@ -8,7 +8,10 @@ import android.location.LocationManager
 import android.location.LocationListener
 import android.location.Location
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -25,9 +28,7 @@ class MainActivity : AppCompatActivity(), LocationListener
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         ActivityCompat.requestPermissions(this, arrayOf<String>(Manifest.permission.READ_EXTERNAL_STORAGE ), 0)
-
         Resources.poiDb = PointOfInterestDatabase.getDatabase(application)
         Resources.context = this
         try{
@@ -39,7 +40,6 @@ class MainActivity : AppCompatActivity(), LocationListener
         } catch(e: Exception){
             Resources.pointsOfInterestList = arrayListOf<PointOfInterest>()
         }
-
 
         this.requestLocation()
 
@@ -81,6 +81,12 @@ class MainActivity : AppCompatActivity(), LocationListener
         }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean
+    {
+        menuInflater.inflate(R.menu.activity_menu, menu)
+        return true
+    }
+
     fun requestLocation() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
         {
@@ -91,6 +97,19 @@ class MainActivity : AppCompatActivity(), LocationListener
         {
             ActivityCompat.requestPermissions(this, arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION), 0)
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId)
+        {
+            R.id.FindClosePOIs ->
+            {
+                val intent = Intent(this, PoiDescriptionActivity::class.java)
+                startActivityForResult(intent, 0)
+                return true
+            }
+        }
+        return false
     }
 
     override fun onLocationChanged(location: Location) {
---
+++ app/src/main/java/com/example/week9/network/jacobdale_com527_ae1/PoiDescriptionActivity.kt
@@ -0,0 +1,8 @@
+package com.example.week9.network.jacobdale_com527_ae1
+
+import androidx.appcompat.app.AppCompatActivity
+
+class PoiDescriptionActivity : AppCompatActivity()
+{
+
+}
