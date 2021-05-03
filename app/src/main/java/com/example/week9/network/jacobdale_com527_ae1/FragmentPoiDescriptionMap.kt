de379d4757969f1994eba666c2c4b22403c240c7
JSDale <57218102+JSDale@users.noreply.github.com>
2021-05-03 12:32:03 +0100

    started working on the activity to have two fragements a map and a recylcer list

---
+++ app/src/main/java/com/example/week9/network/jacobdale_com527_ae1/FragmentPoiDescriptionMap.kt
@@ -0,0 +1,66 @@
+package com.example.week9.network.jacobdale_com527_ae1
+
+import android.os.Bundle
+import android.preference.PreferenceManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import org.osmdroid.config.Configuration
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+
+class FragmentPoiDescriptionMap : Fragment()
+{
+    private lateinit var mapView: MapView
+    private lateinit var mainMapLocationHandler: MainMapLocationHandler
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) : View?
+    {
+        return inflater?.inflate(R.layout.fragment_main_map, container, false)
+    }
+
+    override fun onResume()
+    {
+        super.onResume()
+        Resources.context = context
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
+    {
+        Resources.context = context
+
+        view?.apply{
+            Configuration.getInstance().load(context, PreferenceManager.getDefaultSharedPreferences(context))
+
+            val btnSetZoomLevel = findViewById<Button>(R.id.btnSetZoom)
+            val editTextZoomLevel = findViewById<EditText>(R.id.editTextZoomLevel)
+
+            btnSetZoomLevel.setOnClickListener{setZoomLevel(editTextZoomLevel)}
+
+            var zoomLevel = 16
+            mapView = findViewById<MapView>(R.id.mainMap)
+            mapView.controller.setZoom(zoomLevel)
+            mapView.controller.setCenter(GeoPoint(Resources.latitude, Resources.longitude))
+        }
+
+    }
+
+    private fun setZoomLevel(editText: EditText)
+    {
+        try
+        {
+            view?.apply{
+                mapView.controller.setZoom(editText.text.toString().toInt())
+            }
+        }
+        catch(e: Exception)
+        {
+            Toast.makeText(Resources.context, "Enter Zoom Number", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
--- app/src/main/java/com/example/week9/network/jacobdale_com527_ae1/PoiDescriptionActivity.kt
+++ app/src/main/java/com/example/week9/network/jacobdale_com527_ae1/PoiDescriptionActivity.kt
@@ -1,8 +1,13 @@
 package com.example.week9.network.jacobdale_com527_ae1
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class PoiDescriptionActivity : AppCompatActivity()
 {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
 
+    }
 }
